80bd6bb02d531335b407fd156515fcb3ca76e152
Justice Appiah <jappiah54@gmail.com>
2018-10-14 17:13:48 +0000

    reorganised structure of react pieces, into pages folder

--- src/frontend/pages/app/app.test.js
+++ src/frontend/pages/app/app.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
 import App from './App';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<App/>, div);
+	
+	const component = renderer.create(<App/>);
+	let tree = component.toJSON();
+	
+	console.log(tree);
+	expect(tree).toMatchSnapshot();
 });
